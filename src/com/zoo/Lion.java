31ecde026159d66c21e792a97b001e3b2464f7bf
YahorDanchanka <danchenko.dev@yandex.by>
2022-04-21 11:36:07 +0300

    Added colors

--- src/com/zoo/Lion.java
+++ src/com/zoo/Lion.java
@@ -1,12 +1,18 @@
 package com.zoo;
 
 public class Lion {
+    public static final String ORANGE = "orange";
+    public static final String RED = "red";
+    public static final String YELLOW = "yellow";
+
     public float height;
     public float weight;
+    public String color;
 
-    public Lion(float height, float weight) {
+    public Lion(float height, float weight, String color) {
         this.height = height;
         this.weight = weight;
+        this.color = color;
     }
 
     public void eat(float food)
--- src/com/zoo/Main.java
+++ src/com/zoo/Main.java
@@ -3,7 +3,7 @@ package com.zoo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Добро пожаловать в зоопарк");
-        Lion lion = new Lion(20, 60);
+        Lion lion = new Lion(20, 60, Lion.ORANGE);
         lion.growl();
         System.out.println(lion.weight);
         lion.eat(10);
