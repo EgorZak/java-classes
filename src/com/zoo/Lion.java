721819e22826294e49403e2bb45fdeab53598db0
YahorDanchanka <danchenko.dev@yandex.by>
2022-04-21 11:50:17 +0300

    Added getters & setters

--- src/com/zoo/Lion.java
+++ src/com/zoo/Lion.java
@@ -1,15 +1,39 @@
 package com.zoo;
 
 public class Lion {
-    public enum Colors {RED, ORANGE, YELLOW};
+    public enum Colors {RED, ORANGE, YELLOW}
 
-    public float height;
-    public float weight;
-    public Colors color;
+    private float height;
+    private float weight;
+    private Colors color;
 
     public Lion(float height, float weight, Colors color) {
         this.height = height;
         this.weight = weight;
+        this.color = color;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public Colors getColor() {
+        return color;
+    }
+
+    public void setColor(Colors color) {
         this.color = color;
     }
 
--- src/com/zoo/Main.java
+++ src/com/zoo/Main.java
@@ -5,8 +5,9 @@ public class Main {
         System.out.println("Добро пожаловать в зоопарк");
         Lion lion = new Lion(20, 60, Lion.Colors.ORANGE);
         lion.growl();
-        System.out.println(lion.weight);
-        lion.eat(10);
-        System.out.println(lion.weight);
+
+        System.out.println(lion.getWeight());
+        lion.setWeight(123);
+        System.out.println(lion.getWeight());
     }
 }
